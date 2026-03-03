105ea764fc509e47b09e26be245d6c153682e4cf
ViniciusAguiar3106 <viniciusaguiar6869@gmail.com>
2026-03-03 11:15:55 -0300

    03/03

--- Clinica-main/src/main/java/br/com/senai/clinica/entity/Dono.java
+++ Clinica-main/src/main/java/br/com/senai/clinica/entity/Dono.java
@@ -2,7 +2,6 @@ package br.com.senai.clinica.entity;
 
 import java.util.List;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
